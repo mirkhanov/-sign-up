2f919849cfff86e8f70b758651a4ac5d1669c86d
mirkhanov <mirhanov.m.m@gmail.com>
2019-06-29 00:15:24 +0500

    added slide box

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-function openMenu(){
-    document.getElementById("menu").classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var menu = document.getElementById('menu');
+  menu.onclick = function(e){
+      if(menu.classList.contains('hideBox')){
+          menu.classList.remove('hideBox');
+          menu.classList.add('showBox');
+      }else if(menu.classList.contains('showBox')){
+          menu.classList.remove('showBox');
+          menu.classList.add('hideBox');
+      }else{
+          menu.classList.add('hideBox');
+      }
+      
+  }
+});
